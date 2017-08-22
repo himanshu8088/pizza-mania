1f389d43e8b69d08ae816062cbe777d51f4c3ef3
Himanshu Soni <hsoni@tavisca.com>
2017-08-22 14:22:36 +0530

    renamed test-method name

--- PizzaShop/PizzaShopFixture/PizzaShopFixture.cs
+++ PizzaShop/PizzaShopFixture/PizzaShopFixture.cs
@@ -11,12 +11,23 @@ namespace PizzaShopFixture
     public class PizzaShopFixture
     {
         [Fact]
-        public void Is_item_adding_up_onto_cart()
+        public void Item_adding_up_onto_cart()
         {
             ShoppingCart cart = new ShoppingCart(2);
             PizzaShop shop = new PizzaShop(cart);
             shop.PickItem(new VegPizza());
             Assert.Equal(cart.ItemList.Count,1);
-        }       
+        }
+        [Fact]
+        public void Item_removing_up_from_cart()
+        {
+            ShoppingCart cart = new ShoppingCart(2);
+            PizzaShop shop = new PizzaShop(cart);
+            IItem item = new VegPizza();
+            shop.PickItem(item);
+            shop.DropItem(item);
+            Assert.Equal(cart.ItemList.Count, 0);
+        }
+
     }
 }
